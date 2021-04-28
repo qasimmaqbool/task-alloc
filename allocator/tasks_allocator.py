2445ba6e41bc06fa22e6cdabee7640d40faf80bf
Qasim Maqbool <qasim.maqbool@mysense.ai>
2021-04-28 05:26:59 +0500

    add comments

--- allocator/tasks_allocator.py
+++ allocator/tasks_allocator.py
@@ -16,8 +16,8 @@ class TasksAllocator(object):
     @staticmethod
     def get_next_employee(num_tasks_allocate):
         for k, v in num_tasks_allocate.items():
-            if v > 0:
-                num_tasks_allocate[k] = v - 1
+            if v > 0:  # this indicates there is capacity available for employee
+                num_tasks_allocate[k] = v - 1  # decrement capacity by 1
                 return k
 
     @staticmethod
@@ -25,13 +25,17 @@ class TasksAllocator(object):
         total_tasks_count = len(tasks_df)
         employee_names = list(employees.keys())
         num_employees = len(employee_names)
+
+        # The number of tasks each employee(user) should have
         per_user_tasks = total_tasks_count / num_employees
-        num_tasks_allocate = {}
+        num_tasks_allocate = {}  # this holds the capacity of each employee to be given unallocated tasks
         for employee in employee_names:
+            # Subtract already allocated tasks from number of tasks each employee should have
             num_tasks_allocate[employee] = int(per_user_tasks - tasks_df['user'].value_counts()[employee])
 
         for index, row in tasks_df.iterrows():
             if row['user'] == 'None':
+                # For each unallocated task, pick the next available employee and allocate
                 row['user'] = TasksAllocator.get_next_employee(num_tasks_allocate)
         return tasks_df
 
@@ -42,19 +46,20 @@ class TasksAllocator(object):
         tasks_per_type = tasks_df['type'].value_counts()
         for v in employees.values():
             per_employee_type_tasks[v] = int(tasks_per_type[v] / employee_type_counts[v])
-        grouped_employees = {}
+
+        grouped_employees = {}  # group employees by their type
         for employee_name, employee_type in employees.items():
             if employee_type in grouped_employees:
                 grouped_employees[employee_type].append(employee_name)
             else:
                 grouped_employees[employee_type] = [employee_name]
 
-        correct_tasks = []
+        correct_tasks = []  # determine which tasks are correctly allocated and shouldn't be modified
         for index, row in tasks_df.iterrows():
             if row['user'] != 'None' and row['user'] in grouped_employees[row['type']]:
                 correct_tasks.append(row)
         correct_tasks_df = pd.DataFrame(correct_tasks)
-        num_tasks_allocate = {}
+        num_tasks_allocate = {}  # this holds the capacity of each employee to be given unallocated tasks
         for employee_name, employee_type in employees.items():
             num_tasks_allocate[employee_name] = int(per_employee_type_tasks[employee_type] - correct_tasks_df['user'].value_counts()[employee_name])
 
